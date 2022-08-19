0930eef6f0328ab34793ffe035f9f646b898f356
unknown <jaimeperaza2001@gmail.com>
2022-08-18 20:08:58 -0500

    Get local time

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <time.h>
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+const long offset = 0;
+const int daylight = 3600;
+
+char *ssid = "Usergioarboleda";
+char *password = "ceskqyw2012";
+char *ntpServer = "pool.ntp.org";
+
+void imprimirHora() {
+  struct tm timeInfo;
+  if(!getLocalTime(&timeInfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeInfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void setup() { 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -10,5 +34,7 @@ void loop() {
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
-  Serial.print("entro");
+  Serial.print("Entró al loop");  
+  configTime(offset, daylight, ntpServer);
+  imprimirHora();
 }

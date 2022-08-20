5a2c3689d3af6f0162211799f9bc02cfa1d34d81
Andrewslaxe <jaimeperaza2001@gmail.com>
2022-08-19 21:36:36 -0500

    Fix the offset variable
    Change the value of the offset to get the real date in Colombia

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,36 @@
 #include <WiFi.h>
 #include <time.h>
 
-const long offset = 0;
-const int daylight = 3600;
+const long offset_sec = -18000;
+const int daylight = 0;
 
 char *ssid = "Usergioarboleda";
 char *password = "ceskqyw2012";
 char *ntpServer = "pool.ntp.org";
+
+void imprimirhora();
+
+void setup() { 
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting to WiFi... \n");
+  }
+  Serial.println("Connected to the WiFi network");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());  
+  configTime(offset_sec, daylight, ntpServer);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  imprimirHora();
+}
 
 void imprimirHora() {
   struct tm timeInfo;
@@ -16,25 +40,4 @@ void imprimirHora() {
     return;
   }
   Serial.println(&timeInfo, "%A, %B %d %Y %H:%M:%S");
-}
-
-void setup() { 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  Serial.print("Entró al loop");  
-  configTime(offset, daylight, ntpServer);
-  imprimirHora();
 }
